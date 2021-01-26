3149f8e2bd33c8938a52166516db5c0b819c9b4b
hoytk6 <hoytk@knights.ucf.edu>
2021-01-26 18:50:34 -0500

    finish front page css

--- src/containers/FrontPage.js
+++ src/containers/FrontPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import placeholder from '../assets/placeholder.png';
+import beach from '../assets/beach.png';
+import pearl from '../assets/pearl.png';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
@@ -16,6 +17,7 @@ const styles = {
         height: '400px',
         textAlign: 'center',
         justifyContent: 'center',
+        backgroundImage: 'linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%), url(' + beach + ')',
     },
     downloadButton: {
         display: 'flex',
@@ -24,7 +26,7 @@ const styles = {
         color: 'white',
         fontSize: '20px',
         margin: '0px',
-        bottom: '0',
+        bottom: '10px',
     },
     intro: {
         display: 'flex',
@@ -34,21 +36,60 @@ const styles = {
         margin: '0px',
         left: '10px',
     },
-    introContainer: {
-
-    },
     description: {
         display: 'flex',
         position: 'absolute',
-        color: '#1D70EB',
+        color: 'black',
         fontSize: '40px',
         margin: '0px',
         left: '50px',
         top: '100px',
     },
-    infoContainer: {
-
+    descriptionAlt: {
+        color: '#1D70EB',
+        fontWeight: 'bold',
+        display: 'contents',
     },
+    title: {
+        color: 'black',
+        fontSize: '70px',
+        marginTop: '10px',
+        marginBottom: '10px',
+        textDecoration: 'underline',
+    },
+    infoContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    info: {
+        display: 'flex',
+        alignItems: 'center',
+        fontSize: '30px',
+        margin: '30px',
+    },
+    column: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    featuresContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    featuresButton: {
+        display: 'flex',
+        backgroundColor: '#1D70EB',
+        color: 'white',
+        fontSize: '20px',
+        margin: '0px',
+        justifyContent: 'center',
+    },
+    pearl: {
+        height: '50px',
+        width: '50px',
+        verticalAlign: 'center',
+        marginRight: '10px',
+    }
 };
 
 // TODO: float the text and button over the image
@@ -57,12 +98,40 @@ class FrontPage extends React.Component {
         return(
             <div className={this.props.classes.root}>
                 <div className={this.props.classes.image}>
-                    <img src={placeholder} alt='placeholder' className={this.props.classes.image}/>
+                    <div className={this.props.classes.image}/>
                     <p className={this.props.classes.intro}>SEESHELLS</p>
-                    <p className={this.props.classes.description}>A digital forensics tool for analyzing Windows Registry Artifacts.</p>
+                    <p className={this.props.classes.description}>
+                        A <span className={this.props.classes.descriptionAlt}>digital forensics tool</span> for analyzing Windows Registry Artifacts.
+                    </p>
                     <Button className={this.props.classes.downloadButton}>GET THE TOOL</Button>
                 </div>
+                <div className={this.props.classes.featuresContainer}>
+                    <p className={this.props.classes.title}>Why SeeShells?</p>
+                </div>
                 <div className={this.props.classes.infoContainer}>
+                    <div className={this.props.classes.column}>
+                        <div className={this.props.classes.info}>
+                            <img src={pearl} alt='pearl' className={this.props.classes.pearl}/>
+                            <span className={this.props.classes.descriptionAlt}>Quick parsing</span> of Windows Registry artifacts
+                        </div>
+                        <div className={this.props.classes.info}>
+                            <img src={pearl} alt='pearl' className={this.props.classes.pearl}/>
+                            <span className={this.props.classes.descriptionAlt}>Analysis of artifacts</span> with suspicious behavior flagging
+                        </div>
+                    </div>
+                    <div className={this.props.classes.column}>
+                        <div className={this.props.classes.info}>
+                            <img src={pearl} alt='pearl' className={this.props.classes.pearl}/>
+                            <span className={this.props.classes.descriptionAlt}>Timeline View</span> for a holistic view of computer activity
+                        </div>
+                        <div className={this.props.classes.info}>
+                            <img src={pearl} alt='pearl' className={this.props.classes.pearl}/>
+                            <span className={this.props.classes.descriptionAlt}>Filtering</span> for specific activities and trends
+                        </div>
+                    </div>
+                </div>
+                <div className={this.props.classes.featuresContainer}>
+                    <Button className={this.props.classes.featuresButton}>SEE ALL FEATURES</Button>
                 </div>
             </div>
         );
