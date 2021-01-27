d23af60c0ff64d24b64f2d0aadd67ec971985237
hoytk6 <hoytk@knights.ucf.edu>
2021-01-27 15:48:21 -0500

    create initial admin page

---
+++ src/containers/AdminPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+
+}
+
+class AdminPage extends React.Component {
+    render() {
+        return(
+            
+        );
+    }
+}
+
+export default withStyles(styles)(AdminPage);
