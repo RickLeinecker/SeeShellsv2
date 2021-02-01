3e0ab7029e08c6d2e66156eb249fc9ca61c2c808
hoytk6 <hoytk@knights.ucf.edu>
2021-01-31 19:40:09 -0500

    create placeholders for all pages

--- websiteV2/src/App.js
+++ websiteV2/src/App.js
@@ -1,5 +1,9 @@
 import MenuBar from './components/MenuBar.js';
 import FrontPage from './containers/FrontPage.js';
+import AboutPage from './containers/AdminPage.js';
+import DownloadPage from './containers/DownloadPage.js';
+import DocumentationPage from './containers/DocumentationPage.js';
+import DeveloperPage from './containers/DeveloperPage.js';
 import AdminPage from './containers/AdminPage.js';
 import { withStyles } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -25,6 +29,18 @@ function App(props) {
         <div className={props.classes.content}>
           <Route exact path="/">
             <FrontPage/>
+          </Route>
+          <Route path="/about">
+            <AboutPage/>
+          </Route>
+          <Route path="/download">
+            <DownloadPage/>
+          </Route>
+          <Route path="/documentation">
+            <DocumentationPage/>
+          </Route>
+          <Route path="/developers">
+            <DeveloperPage/>
           </Route>
           <Route path="/admin">
             <AdminPage/>
