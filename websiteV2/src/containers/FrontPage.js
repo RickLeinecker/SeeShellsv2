714d1b83fe2c2d236fe65bfd68d163ea06f1fb81
hoytk6 <hoytk@knights.ucf.edu>
2021-02-28 19:17:39 -0500

    fix animation overflow

--- websiteV2/src/containers/FrontPage.js
+++ websiteV2/src/containers/FrontPage.js
@@ -23,6 +23,7 @@ const styles = {
         textAlign: 'center',
         justifyContent: 'center',
         backgroundImage: 'url(' + beach + ')',
+        overflow: 'hidden',
     },
     downloadButton: {
         display: 'flex',
@@ -112,7 +113,7 @@ const styles = {
         width: '50px',
         verticalAlign: 'center',
         marginRight: '10px',
-    }
+    },
 };
 
 class FrontPage extends React.Component {
